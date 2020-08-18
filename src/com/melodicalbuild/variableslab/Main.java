0aa554f90a8b86a2d5fc88d1e3f6faf5f39a2696
Vixel <43552157+MelodicAlbuild@users.noreply.github.com>
2020-08-18 17:09:06 -0500

    Add files via upload

---
+++ src/com/melodicalbuild/variableslab/Main.java
@@ -0,0 +1,42 @@
+package com.melodicalbuild.variableslab;
+
+public class Main
+{
+   public static void main ( String[] args )
+   {
+	//define 1 variable of each of the
+	//following data types
+	//byte	short		int 		long
+	//float	double
+	//char      boolean	String
+
+	//integer variables
+	byte byteOne = 127;
+
+	System.out.println("/////////////////////////////////");
+	System.out.println("*Alex Drum              07/18/02*");
+	System.out.println("*                               *");
+	System.out.println("*        integer types          *");
+	System.out.println("*                               *");
+	System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+	System.out.println("*16 bit - shortOne = -32123     *");
+	System.out.println("*32 bit - intOne = 90877        *");
+	System.out.println("*64 bit - longOne = 999999999   *");
+	System.out.println("*                               *");
+	System.out.println("*         real types            *");
+	System.out.println("*                               *");
+	System.out.println("*32 bit - floatOne = 38.5678    *");
+	System.out.println("*64 bit - doubleOne = 923.234   *");
+	System.out.println("*                               *");
+	System.out.println("*    other integer types        *");
+	System.out.println("*                               *");
+	System.out.println("*16 bit - charOne = A           *");
+	System.out.println("*                               *");
+	System.out.println("*        other types            *");
+	System.out.println("*                               *");
+	System.out.println("*booleanOne = true              *");
+	System.out.println("*stringOne = hello world        *");
+	System.out.println("/////////////////////////////////");
+   }
+}
+
